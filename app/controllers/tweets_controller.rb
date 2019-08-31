932c2a9e47e5ca50ab7211fb6930c06c85d6970a
takumi14 <03serious@gmail.com>
2019-08-31 20:30:56 +0900

    投稿機能実装

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -7,12 +7,17 @@ class TweetsController < ApplicationController
   end
 
   def new
-
+    
   end
 
+  def create
+    Tweet.create(tweet_params)
+  end
 
-
-
+  private
+  def tweet_params
+    params.permit(:name, :image, :text)
+  end
 
 
 end
