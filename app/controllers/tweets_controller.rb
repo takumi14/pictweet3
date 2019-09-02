c9c681699469f49524908d644e3ae633588e6631
takumi14 <03serious@gmail.com>
2019-09-02 16:57:10 +0900

    user unlogin to index view
    ユーザーがログインしていない場合
    強制的にindexに飛ぶよう設定

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,6 +1,6 @@
 class TweetsController < ApplicationController
 
-
+  before_action :move_to_index, except: :index
 
   def index
     @tweets = Tweet.order("created_at DESC").page(params[:page]).per(5)
@@ -19,5 +19,8 @@ class TweetsController < ApplicationController
     params.permit(:name, :image, :text)
   end
 
+  def move_to_index
+    redirect_to action: :index unless user_signed_in?
+  end
 
 end
