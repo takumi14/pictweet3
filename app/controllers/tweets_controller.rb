20f163d7001aec730aec2a88e4384e5ec768c9c7
takumi14 <03serious@gmail.com>
2019-09-03 16:02:06 +0900

    comment function create

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -32,6 +32,7 @@ class TweetsController < ApplicationController
 
   def show
     @tweet = Tweet.find(params[:id])
+    @comments = @tweet.comments.includes(:user)
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'tweets#index'
-  get 'tweets' => 'tweets#index'
-  get 'tweets/new' => 'tweets#new'
-  post 'tweets' => 'tweets#create'
-  delete  'tweets/:id'  => 'tweets#destroy'
-  get   'tweets/:id/edit'  => 'tweets#edit'
-  patch   'tweets/:id'  => 'tweets#update'
-  get 'users/:id' => 'users#show'
-  get 'tweets/:id' => 'tweets#show'
+  resources :tweets do                    #tweets_controllerに対してのresourcesメソッド
+    resources :comments, only: [:create]
+  end
+  resources :users, only: [:show] 
 end
